f60593061b65a520059d2cdd3cc584ba5425ffd6
viveksasumahanthi-12 <viveksasumahanthi@gmail.com>
2025-07-13 22:36:19 +0530

    Create 1.js

---
+++ js tasks/task 13/1.js
@@ -0,0 +1,22 @@
+let animal = {
+  type: "Dog",
+  name: "Bruno",
+  age: 4,
+  sound: function() {
+    console.log("Woof!");
+  }
+};
+
+animal.sound();
+
+
+let car = {
+  brand: "Toyota",
+  model: "Camry",
+  year: 2022,
+  start: function() {
+    console.log("Engine started.");
+  }
+};
+
+car.start();
