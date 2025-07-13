e4b33e40eba7ec092cd744beb2510a31c9d4c472
viveksasumahanthi-12 <viveksasumahanthi@gmail.com>
2025-07-13 22:45:57 +0530

    Create 4.js

---
+++ js tasks/task 13/4.js
@@ -0,0 +1,8 @@
+let numbers = [4, 29, 1, 100, 56, -3, 7];
+
+// Find highest and lowest using Math methods
+let highest = Math.max(...numbers);
+let lowest = Math.min(...numbers);
+
+console.log("Highest:", highest);
+console.log("Lowest:", lowest);
