5b7e05a62fec0a9926ccc373bf59efcfb724b582
viveksasumahanthi-12 <viveksasumahanthi@gmail.com>
2025-07-14 10:45:54 +0530

    Create 5.js

---
+++ js tasks/task 16/5.js
@@ -0,0 +1,8 @@
+let numbers = [1, 9, 8];
+let sum = 0;
+for (let i = 0; i < numbers.length; i++) {
+  sum += numbers[i];
+}
+let average = sum / numbers.length;
+console.log("Sum:", sum);
+console.log("Average:", average);
