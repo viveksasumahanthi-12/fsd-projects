42753428ac56f045247c7661edbc11352724d9c4
viveksasumahanthi-12 <viveksasumahanthi@gmail.com>
2025-07-14 10:15:48 +0530

    Create 2.js

---
+++ js tasks/task 15/2.js
@@ -0,0 +1,8 @@
+let sentence = "I am learning js";
+let subWord = "js";
+
+if (sentence.includes(subWord)) {
+  console.log(`'${subWord}' exists in the string.`);
+} else {
+  console.log(`'${subWord}' does NOT exist in the string.`);
+}
